4f48b601c123a670556b03e5cd7e58afc170d519
YUSRI ADIB <muhdyusriadib@gmail.com>
2025-08-05 00:44:56 +0800

    Update google_logger.py

--- google_logger.py
+++ google_logger.py
@@ -1,17 +1,19 @@
+# src/google_logger.py
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 import os
 import json
 
-def log_to_google_sheet(wallet, asset_type, score, status):
+def log_scan(wallet, asset_type, score, status):
     try:
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
         creds_json = json.loads(os.environ.get("GOOGLE_CREDENTIALS_JSON"))
         creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
         client = gspread.authorize(creds)
 
-        sheet = client.open("AssetGuard_Logs").sheet1  # Name of your sheet
+        sheet = client.open("AssetGuard_Logs").sheet1  # Nama Google Sheet
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         row = [now, wallet, asset_type, str(score), status]
